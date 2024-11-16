91d784f6eb66d0f8658af02b63a09310cb154365
Vincent 🌿 <165411846+spacebagel@users.noreply.github.com>
2024-11-16 09:34:13 +0300

    .

---
+++ scripts/context.js
@@ -0,0 +1,74 @@
+var dishes = [
+    {
+        keyword: 'kimchi',
+        name: 'Суп кимчи',
+        price: 450,
+        category: 'soup',
+        weight: 330,
+        image: '../imgs/dishes/soup-1.jpg'
+    },
+    {
+        keyword: 'bachchi',
+        name: 'Суп бахчи',
+        price: 550,
+        category: 'soup',
+        weight: 320,
+        image: '../imgs/dishes/soup-2.jpeg'
+    },
+    {
+        keyword: 'harchi',
+        name: 'Суп харчи',
+        price: 600,
+        category: 'soup',
+        weight: 330,
+        image: '../imgs/dishes/soup-3.jpeg'
+    },
+    {
+        keyword: 'wok-tai',
+        name: 'Wok Тайская креветка',
+        price: 550,
+        category: 'main-dish',
+        weight: 330,
+        image: '../imgs/dishes/main-dish-1.jpeg'
+    },
+    {
+        keyword: 'wok-pan',
+        name: 'Wok паназиатский',
+        price: 300,
+        category: 'main-dish',
+        weight: 310,
+        image: '../imgs/dishes/main-dish-2.jpeg'
+    },
+    {
+        keyword: 'wok-classic',
+        name: 'Wok классический',
+        price: 445,
+        category: 'main-dish',
+        weight: 330,
+        image: '../imgs/dishes/main-dish-3.jpeg'
+    },
+    {
+        keyword: 'dragon-blood',
+        name: 'Кровь дракона',
+        price: 700,
+        category: 'drink',
+        weight: 330,
+        image: '../imgs/dishes/drink-1.jpeg'
+    },
+    {
+        keyword: 'old-drink',
+        name: 'Столетняя жижа',
+        price: 800,
+        category: 'drink',
+        weight: 330,
+        image: '../imgs/dishes/drink-2.jpeg'
+    },
+    {
+        keyword: 'acetone-drink',
+        name: 'Ацетон',
+        price: 430,
+        category: 'drink',
+        weight: 330,
+        image: '../imgs/dishes/drink-3.jpeg'
+    }
+];
---
+++ scripts/dish-list-fill.js
@@ -0,0 +1,72 @@
+let totalCost = 0;
+const selectedDishes = { soup: null, 'main-dish': null, drink: null };
+
+function updateTotalCost() {
+    const totalCostElement = document.getElementById('total-cost');
+    if (totalCostElement) {
+        totalCostElement.textContent = `${totalCost} ₽`;
+    }
+}
+
+function renderDishes() {
+    const categories = {
+        soup: 'Выберите суп',
+        'main-dish': 'Выберите главное блюдо',
+        drink: 'Выберите напиток'
+    };
+
+    const container = document.querySelector('#dishes-container');
+    container.innerHTML = '';
+
+    Object.keys(categories).forEach(category => {
+        const section = document.createElement('section');
+        const heading = document.createElement('h2');
+        heading.textContent = categories[category];
+        section.appendChild(heading);
+
+        const dishContainer = document.createElement('div');
+        dishContainer.className = 'lunch-dish-container';
+
+        const sortedDishes = dishes
+            .filter(dish => dish.category === category)
+            .sort((a, b) => a.name.localeCompare(b.name));
+
+        sortedDishes
+            .filter(dish => dish.category === category)
+            .forEach(dish => {
+                const dishCard = document.createElement('div');
+                dishCard.className = 'dish-card';
+
+                dishCard.innerHTML = `
+                    <img src="${dish.image}" alt="${dish.name}">
+                    <p class="dish-cost">${dish.price}</p>
+                    <p class="dish-title">${dish.name}</p>
+                    <p class="dish-weight">${dish.weight} г</p>
+                    <button class="select-dish-button">Добавить</button>
+                `;
+
+                dishCard.addEventListener('click', () => {
+                    if (selectedDishes[category] === dish) return;
+                    if (selectedDishes[category]) {
+                        totalCost -= parseFloat(selectedDishes[category].price);
+                    }
+
+                    selectedDishes[category] = dish;
+                    totalCost += parseFloat(dish.price);
+                    updateTotalCost();
+                    const categoryLabelId = `${category}-selected`;
+                    const categoryLabel = document.querySelector(`#${categoryLabelId}`);
+                    if (categoryLabel) {
+                        categoryLabel.textContent = dish.name + ' ' + dish.price + ' ₽';
+                    }
+                });
+
+                dishContainer.appendChild(dishCard);
+            });
+
+        section.appendChild(dishContainer);
+        container.appendChild(section);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', renderDishes);
